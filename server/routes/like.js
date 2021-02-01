b4d34c322cf2be9ff28c27a0b33b71dc188bd19d
이동영 <leedy2491@ajou.ac.kr>
2021-02-01 18:26:46 +0900

    Thank you for visiting dev2d0's Github.

--- server/routes/like.js
+++ server/routes/like.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { Like } = require("../models/Like");
 
-const { auth } = require("../middleware/auth");
-
 //=================================
 //             Like
 //=================================
